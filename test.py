e1c52e415b7a4d02c5d69197912d729216b38015
Matthew Johnson <mattjj@csail.mit.edu>
2013-01-13 23:57:23 -0500

    made test a lot more interesting

--- test.py
+++ test.py
@@ -5,20 +5,24 @@ from matplotlib import pyplot as plt
 import Kalman
 from Gaussians import Gaussian
 
-x = np.sin(np.linspace(0,8*np.pi,100))
-y = np.cos(np.linspace(0,8*np.pi,100))
-data = (np.vstack((x,y))*np.arange(100.)).T
-
-A = np.eye(2)
-B = 2*np.eye(2)
+theta = 0.1*np.pi
+A = 1.01*np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+B = 0.1*np.eye(2)
 C = np.eye(2)
-D = np.eye(2)
+D = 0.5*np.eye(2)
+
+x = np.zeros((100,2))
+x[0] = (1,1)
+for i in range(1,100):
+    x[i] = A.dot(x[i-1]) + B.dot(np.random.randn(2))
+
+y = (C.dot(x.T) + D.dot(np.random.normal(size=x.shape).T)).T
 
 initial_distn = Gaussian(np.zeros(2),np.eye(2))
 
-smoothed_distns = Kalman.smooth(A,B,C,D,initial_distn,data)
+smoothed_distns = Kalman.smooth(A,B,C,D,initial_distn,y)
 mus = np.array([d.mu for d in smoothed_distns])
 
-plt.plot(data[:,0],data[:,1],'bx-')
+plt.plot(y[:,0],y[:,1],'bx-')
 plt.plot(mus[:,0],mus[:,1],'r.-')
 plt.show()
